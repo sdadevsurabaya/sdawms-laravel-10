ff04be33164c897fcb285fd05fcf7c541164cc5b
alfin <fachrizalalfin@gmail.com>
2025-04-15 15:48:24 +0700

    perapian layout dashboard dan sidebar

--- resources/views/back/layouts/layout.blade.php
+++ resources/views/back/layouts/layout.blade.php
@@ -4,78 +4,13 @@
     @include('back.layouts.title-meta')
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- font awesome icon --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    {{-- <link rel="stylesheet" href="css/custom.css"> --}}
+    {{-- <link rel="stylesheet" href="/assets/css/custom.css"> --}}
+    <link rel="stylesheet" href="/assets/css/custom.css">
     <style>
-        html, body {
-            height: 100%;
-            margin: 0;
-        }
 
-        body {
-            background-image: url('{{ asset("images/login-bg.jpg") }}');
-            display: flex;
-            flex-direction: column;
-        }
-
-        .wrapper {
-            flex: 1;
-            display: flex;
-            flex-direction: row;
-            flex-wrap: wrap;
-            min-height: 0;
-        }
-
-        .sidebar {
-            background-color: #343a40;
-            color: #fff;
-        }
-
-        .sidebar a {
-            color: white;
-            padding: 12px 20px;
-            display: block;
-            text-decoration: none;
-        }
-
-        .sidebar a:hover {
-            background-color: #495057;
-        }
-
-        .content-wrapper {
-            display: flex;
-            flex-direction: column;
-            flex: 1;
-            min-height: 100vh;
-        }
-
-        main {
-            flex: 1;
-            padding: 1rem;
-        }
-
-        footer {
-            background-color: #212529;
-            color: white;
-            padding: 1rem;
-        }
-
-        @media (max-width: 768px) {
-            .sidebar {
-                width: 100%;
-                order: 1;
-            }
-
-            .content-wrapper {
-                order: 2;
-                width: 100%;
-            }
-        }
-
-        @media (min-width: 769px) {
-            .sidebar {
-                width: 250px;
-                min-height: 100vh;
-            }
-        }
     </style>
 </head>
 <body>
--- resources/views/back/layouts/sidebar.blade.php
+++ resources/views/back/layouts/sidebar.blade.php
@@ -1,8 +1,53 @@
-<div class="sidebar">
-    <a href="{{ route('admin.dashboard') }}">Dashboard</a>
-    <a href="{{ route('branch.index') }}">Branches</a> <!-- Link ke halaman Branch -->
-    <a href="{{ route('warehouse.index') }}">Warehouses</a>
-    <a href="{{ route('rack.index') }}">Racks</a>
-    <a href="{{ route('item.index') }}">Items</a>
-    <a href="#">Logout</a>
+<div class="sidebar d-none d-lg-block">
+    <div class="offcanvas-header d-block text-center position-relative py-4">
+        <div class="position-relative d-inline-block mb-1">
+            <img src="/images/300-1.jpg" class="img-thumbnail rounded-circle border-0 bg-dark" width="66" alt=""
+                style="padding:.35rem;">
+        </div>
+
+        <h5 class="offcanvas-title" id="sidebarLabel">Web Developer </h5>
+        <p class="mb-0 small text-secondary">
+            Administrator ID
+        </p>
+        <button type="button" class="btn-close position-absolute top-0 end-0 m-1 d-lg-none" data-bs-dismiss="offcanvas"
+            data-bs-target="#sidebar" aria-label="Close"></button>
+    </div>
+    <ul class="metismenu list-unstyled mm-show">
+        <li class="menu-list">
+            <a href="{{ route('admin.dashboard') }}">
+                <i class="fas fa-search"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="menu-list">
+            <a href="{{ route('branch.index') }}">
+                <i class="fas fa-search"></i>
+                <span>Branches</span>
+            </a> <!-- Link ke halaman Branch -->
+        </li>
+        <li class="menu-list">
+            <a href="{{ route('warehouse.index') }}">
+                <i class="fas fa-search"></i>
+                <span>Warehouses</span>
+            </a>
+        </li>
+        <li class="menu-list">
+            <a href="{{ route('rack.index') }}">
+                <i class="fas fa-search"></i>
+                <span>Racks</span>
+            </a>
+        </li>
+        <li class="menu-list">
+            <a href="{{ route('item.index') }}">
+                <i class="fas fa-search"></i>
+                <span>Items</span>
+            </a>
+        </li>
+        <li class="menu-list">
+            <a href="{{ route('item.index') }}">
+                <i class="fas fa-search"></i>
+                <span>Items</span>
+            </a>
+        </li>
+    </ul>
 </div>
