f46f934e52459ccdeea6c5dbeb58cf7b46007a11
Danu-Nur <diramadhan13@gmail.com>
2025-04-16 14:30:45 +0700

    add SweetAlert2 script for enhanced alert functionality

--- resources/views/back/layouts/layout.blade.php
+++ resources/views/back/layouts/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <style>
         html, body {
             height: 100%;
