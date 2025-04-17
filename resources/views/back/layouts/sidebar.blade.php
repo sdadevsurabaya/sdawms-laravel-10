e24eadcf08fa35faa16ab92969364422e2ab2ef5
Danu-Nur <diramadhan13@gmail.com>
2025-04-17 10:58:05 +0700

    add logout functionality with CSRF token refresh in sidebar

--- resources/views/back/layouts/sidebar.blade.php
+++ resources/views/back/layouts/sidebar.blade.php
@@ -4,5 +4,20 @@
     <a href="{{ route('warehouse.index') }}">Warehouses</a>
     <a href="{{ route('rack.index') }}">Racks</a>
     <a href="{{ route('item.index') }}">Items</a>
-    <a href="#">Logout</a>
+    <a href="#" onclick="event.preventDefault();btnSubmit();">
+        Logout
+    </a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
+    <script>
+        function btnSubmit() {
+            fetch('/refresh-csrf')
+                .then(response => response.json())
+                .then(data => {
+                    document.querySelector('input[name="_token"]').value = data.csrfToken;
+                    document.getElementById('logout-form').submit();
+                });
+        }
+    </script>
 </div>
