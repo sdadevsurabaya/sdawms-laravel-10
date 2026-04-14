d33f859bd3ef3cef0b882524df31dba1a36748f8
Danu <diramadhan13@gmail.com>
2026-04-14 14:25:59 +0700

    feat: migrate font from halvetica to roboto default

--- resources/views/back/layouts/layout.blade.php
+++ resources/views/back/layouts/layout.blade.php
@@ -11,6 +11,12 @@
     <meta name="apple-mobile-web-app-title" content="SDA WMS">
     <link rel="manifest" href="/manifest.json">
     <link rel="apple-touch-icon" href="/images/icon-96.png">
+    
+    {{-- Google Fonts - Roboto --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     {{-- font awesome icon --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
--- resources/views/front/layouts/layout.blade.php
+++ resources/views/front/layouts/layout.blade.php
@@ -6,8 +6,15 @@
     <title>SDAWMS - Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    {{-- Google Fonts - Roboto --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+
     <style>
         body {
+            font-family: 'Roboto', sans-serif;
             /* min-height: 100vh; */
             width: 100%;
             height: 100%;
@@ -15,6 +22,7 @@
             flex-direction: column;
             background-color: #f8f9fa;
         }
+
         main {
             flex: 1;
         }
